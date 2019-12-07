d9f12aab5aff5c856ec4e3eef9e361673b8728e5
jk977 <jdk64@comcast.net>
2019-12-06 19:28:54 -0800

    Fixing Pattern.js documentation

--- js/Pattern.js
+++ js/Pattern.js
@@ -1,20 +1,22 @@
 /**
- * @callback predicate
- * @param {*} value - Value to check.
- * @returns {boolean}
+ * Type of callback passed to arraysEqual.
+ * @callback comparator
+ * @param {*} lhs
+ * @param {*} rhs
+ * @returns {boolean} - Whether or not lhs equals rhs.
  */
 
 /**
  * Compare equality of two arrays.
- *
  * @param {Array<*>} ary1
  * @param {Array<*>} ary2
- * @param {predicate} elemsEqual - Function to compare array elements with. If undefined, `===` is used.
+ * @param {comparator} elemsEqual - Function to compare array elements with. If undefined, `===` is used.
  * @returns {boolean}
  */
 function arraysEqual(ary1, ary2, elemsEqual) {
-  const equals = elemsEqual || ((a, b) => a === b); // use strict equals if elemsEqual isn't provided
-  return ary1.every((elem, idx) => equals(elem, ary2[idx]));
+  const tripleEquals = (a, b) => a === b;
+  const equals = elemsEqual || tripleEquals; // use `===` if elemsEqual isn't provided
+  return ary1.every((elem, i) => equals(elem, ary2[i]));
 }
 
 /*
@@ -24,12 +26,16 @@ function arraysEqual(ary1, ary2, elemsEqual) {
 
 // eslint-disable-next-line no-unused-vars
 class Pattern {
+  /**
+   * Check equality of two patterns.
+   * @param {Array<Array<*>>} p1
+   * @param {Array<Array<*>>} p2
+   * @returns {boolean}
+   */
   static equals(p1, p2) {
     if (p1 === p2) {
       return true;
-    }
-
-    if (p1 === undefined || p2 === undefined) {
+    } else if (p1 === undefined || p2 === undefined) {
       return false;
     }
 
